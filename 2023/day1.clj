56f8afb3138b2ebeb7dfc38e2765a6ccb29f7167
isadora.albrecht <isadora.albrecht@king.com>
2023-12-01 17:07:32 +0100

    Day 1

---
+++ 2023/day1.clj
@@ -0,0 +1,45 @@
+(require '[clojure.string :as str])
+(def input (slurp "input/day1.txt"))
+
+
+(comment "P1 -----------------")
+
+(def sample 
+  (str "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet" ))
+
+(def get-nums (fn [s] (re-seq #"[0-9]" s)))
+(def make-num (fn [s] (str (first s) (last s))))
+(reduce +
+        (map (comp read-string make-num get-nums) (str/split input #"\n")))
+
+
+(comment "P2 -----------------")
+
+(def sample 
+  (str "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"))
+
+(def num-re "zero|one|two|three|four|five|six|seven|eight|nine")
+(def r-forward (re-pattern (str "[0-9]|" num-re)))
+(def r-backward (re-pattern (str "[0-9]|" (str/reverse num-re))))
+
+(def get-num fn[s] (str (re-find r-forward s) (re-find r-backward (str/reverse s))))
+
+(def get-all-nums (fn [s] map (re-seq num-re s)))
+(def word-to-num {"one" 1, "two" 2, "three" 3, "four" 4, "five" 5, "six" 6, "seven" 7, "eight" 8, "nine" 9})
+(def conv-to-num (fn [s] (if (re-matches #"[0-9]" s) s (word-to-num s))))
+(def make-f-l-num (fn [s] (str (conv-to-num (first s)) (conv-to-num (last s)))))
+
+(re-find #"zero|one|two|three|four|five|six|seven|eight|nine|[0-9]" "twone")
+
+(reduce + 
+        (map (comp read-string make-f-l-num get-all-nums) (str/split input #"\n")))
+

7b80747774f51b43f9fa4c5957c039668883ae6d
isadora.albrecht <isadora.albrecht@king.com>
2024-12-06 21:50:24 +0100

    makes it faster]

--- 2024/day6/day6.go
+++ 2024/day6/day6.go
@@ -13,16 +13,16 @@ type Pos struct {
 }
 
 func main() {
-	labMap, maxX, maxY, startPos := parseMap("../_input/day6.txt")
-	println("Map size", maxX, maxY)
-	findLoops(labMap, startPos, Pos{0, -1}, make(map[Pos][]Pos), make(map[Pos]bool), startPos, Pos{0, -1})
+	println("Day 6")
+	labMap, _, _, startPos := parseMap("../_input/day6.txt")
+	findLoops(labMap, startPos, Pos{0, -1}, make(map[Pos][]Pos), make(map[Pos]bool))
 }
 
 func turnRight(dir Pos) Pos {
 	return Pos{-dir.y, dir.x}
 }
 
-func findLoops(labMap map[Pos]string, currPos Pos, dir Pos, visited map[Pos][]Pos, loop_rock map[Pos]bool, start_pos Pos, start_dir Pos) {
+func findLoops(labMap map[Pos]string, currPos Pos, dir Pos, visited map[Pos][]Pos, loop_rock map[Pos]bool) {
 	visited[currPos] = append(visited[currPos], dir)
 	newPos := Pos{currPos.x + dir.x, currPos.y + dir.y}
 	elem, inMap := labMap[newPos]
@@ -30,17 +30,20 @@ func findLoops(labMap map[Pos]string, currPos Pos, dir Pos, visited map[Pos][]Po
 		dir = turnRight(dir)
 		newPos = currPos
 	} else if elem == "." {
-		labMap[newPos] = "#"
-		if walkTillLoop(labMap, start_pos, start_dir, visited, make(map[Pos][]Pos)) {
-			loop_rock[newPos] = true
+		_, hasSeen := visited[newPos]
+		if !hasSeen {
+			labMap[newPos] = "#"
+			if walkTillLoop(labMap, currPos, turnRight(dir), visited, make(map[Pos][]Pos)) {
+				loop_rock[newPos] = true
+			}
+			labMap[newPos] = "."
 		}
-		labMap[newPos] = "."
 	} else if !inMap {
 		fmt.Println(len(visited))   //P1
 		fmt.Println(len(loop_rock)) //P2
 		return
 	}
-	findLoops(labMap, newPos, dir, visited, loop_rock, start_pos, start_dir)
+	findLoops(labMap, newPos, dir, visited, loop_rock)
 }
 
 func walkTillLoop(labMap map[Pos]string, currPos Pos, dir Pos, visited map[Pos][]Pos, n_visited map[Pos][]Pos) bool {
